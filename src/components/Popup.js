92e900c7f2a9db8820e8fae30fed8e3974a2b150
Lakatosska <vizualnoe@gmail.com>
2022-03-01 23:58:45 +0300

    added handleEscClose method

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,21 +1,58 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._closePopupButton = this._popup.querySelector('.popup__close-button');
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   setEventListeners() {
-    console.log('fff')
+    console.log('fff');
   }
 
-  _handleEscClose() {
-    console.log('fff')
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  }
+
+  _handleClose(evt) {
+    if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains(this._closePopupButton)) {
+      this.close()
+    }
   }
 }
+
+/*
+
+// закрытие модального окна по клику на крестик и на оверлей
+const popups = document.querySelectorAll('.popup')
+
+popups.forEach((popup) => {
+  popup.addEventListener('mousedown', (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+      closePopup(popup)
+    }
+      if (evt.target.classList.contains('popup__close-button')) {
+        closePopup(popup)
+      }
+  })
+})
+
+// закрытие модального окна нажатием на ESC
+function closeByEscape(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened')
+    closePopup(openedPopup)
+  }
+}
+*/
